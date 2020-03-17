0b9002578e1713e443bcb0792643821e91959f5c
ybai62868 <ybai62868@gmail.com>
2020-03-17 12:47:55 +0800

    [Add] quickSort

---
+++ basicClass/01-basicAlgorithm/quickSort/quickSort.cpp
@@ -0,0 +1,39 @@
+# include <cstdio>
+# include <iostream>
+# include <algorithm>
+
+using namespace std;
+
+# define MAX 112345
+int a[MAX];
+
+void quick_sort(int* q, int l, int r)
+{
+	if (l >= r) {
+		return;
+	}
+	int x = q[(l + r) >> 1], i = l - 1, j = r + 1;
+	while(i < j) {
+		do i++; while(q[i] < x);
+		do j--; while(q[j] > x);
+		if (i < j) swap(q[i], q[j]);
+	}
+	quick_sort(q, l, j);
+	quick_sort(q, j + 1, r);
+
+}
+
+int main(void)
+{
+	int n; scanf("%d", &n);
+	for (int i = 0;i < n;i++){
+		scanf("%d", &a[i]);
+	}
+	quick_sort(a, 0, n-1);
+	for(int i = 0;i < n;i++) {
+		printf("%d ", a[i]);
+	}
+
+
+	return 0;
+}
